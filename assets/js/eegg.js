3b400827b1a20d0f66b8201b36a29459c81767d5
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-03-25 17:24:31 -0300

    Adds a sector layout to display more information
        Now it's possible to set the workers of an specific sector with the
    frontmatter. The tags worker, picture and contact are available.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- assets/js/eegg.js
+++ assets/js/eegg.js
@@ -11,7 +11,10 @@ let o = {
 
 let h = {
 	de : (n) => {return ty.fromCharCode(n)},
-	k  : (n) => {return n.toUpperCase()}
+	k  : (n) => {return n.toUpperCase()},
+	d  : ()  => {return document},
+	e  : (n, t) => {return n.getElementById(t)},
+	t  : (n, t) => {return n.getElementsByClassName(t)}
 }
 
 let bet = new Map()
@@ -23,19 +26,6 @@ for (var j = 0; j < 26; j++) {
 
 let i = document.getElementById("logo")
 let c = 0
-
-let ka = () => {return "Ga, wm ew uzseg Jsxswd Usehgk Fmfwk w vwkwfngdna wkkw kalw. Lm hgvwk ew wfugfljsj fg kalw jsxswduf.yalzmt.ag"} 
-
-i.addEventListener('click', function() {
-	if (c == 15) {
-		o.trig(r(ka()))
-		c = -1
-	}
-	l(console, c)
-	c++;
-
-}, true)
-
 let l = (c,m) => {c.log(m)}
 let r = (y) => {
 	let ru = ""
@@ -45,7 +35,8 @@ let r = (y) => {
 		let ci = bet[h.k(it(c1, y))]
 		let chr = it(c1, y)
 
-		if (chr === " " || chr === "." || chr === ",") {
+		if (chr === " " || chr === "." || chr === "," || chr === "\'" || chr === "\n" || chr === "?" ||
+			chr === "<" || chr === ">") {
 			c1++
 			ru += chr
 		} else {
@@ -66,3 +57,54 @@ let r = (y) => {
 
 let it = (i,s) => {return h.k(s.charAt(i))}
 let pi = (n) => {return bet[n]}
+
+let typeofun = () => {return 'Zwddg. Eq fsew ak Jsxswd Usehgk Fmfwk sfv A\'e lzw esaf vwnwdghwj gx lzak owtkalw. Ax qgm oakz lg ugflsul' +
+   ' ew mkw lzw xgddgoafy tmllgfk. Lzwjw ak s jwhgkalgjq gx lzak owtkalw sfv ax qgm\'v dacw lg ugfljatmlw bmkl mkw lzw dafc gf lzw' +
+   ' tgllge gx lzak hsyw.<tj><tj>Al oskf\'l lzsl zsjv lg xafv, osk al?'}
+
+let bu = () => {return '<div class="modal" id="mdev"><div class="modal-background"></div><div class="modal-card">' +
+					   '<header id="header" class="modal-card-head"><p class="modal-card-title">Developer information</p>' +
+					   '<button class="delete has-background-danger"></button></header><section class="modal-card-body">' +
+					   r(typeofun()) +
+					   '</section><footer class="modal-card-foot is-right">' +
+					   '<a href="https://rafaelcn.github.io" class="button"><i class="fas fa-fw fa-link "></i> Blog</button>' +
+					   '<a href="mailto:rafaelnunes@engineer.com" class="button"><i class="fa fa-fw fa-envelope"></i> Email</button>' +
+					   '<a href="https://github.com/rafaelcn" class="button"><i class="fab fa-fw fa-github-alt"></i> Github</button></footer></div></div>'}
+
+let onload = () => {
+	let r = h.d().getElementsByTagName('body')[0]
+	let d = h.d().createElement('div')
+	let kj = r.appendChild(d)
+	kj.innerHTML = bu()
+}
+
+onload()
+
+let tr = h.e(h.d(), 'mdev')
+
+h.t(h.d(), 'delete')[0].onclick = event => {
+	tr.classList.add('is-inactive')
+	tr.classList.remove('is-active')
+}
+
+
+h.e(h.d(), 'de')
+tr.onclick = (event) => {
+    if (event.target == tr.querySelector('.modal-background')) {
+        tr.classList.add('is-inactive')
+        tr.classList.remove('is-active')
+    }
+}
+
+i.addEventListener('click', function() {
+	if (c == 8) {
+		//o.trig(r(ka()))
+		let asdfs = h.d()
+		let sd = h.e(asdfs, 'mdev')
+		sd.classList.add('is-active')
+		c = -1
+	}
+	l(console, c)
+	c++;
+
+}, true)
